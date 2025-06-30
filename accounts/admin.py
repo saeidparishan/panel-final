a64c2535a0402eb395ad5edcdf86f924e7531a62
saeidparishan <130152758+saeidparishan@users.noreply.github.com>
2025-06-30 13:18:47 +0330

    completed admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User, Department
+
+
+class UserAdmin(UserAdmin):
+    model = User
+    list_display = ("id","username", "role")
+    list_filter = ("is_staff", "role")
+    fieldsets = (
+        ("Authenticate", {"fields": ("username","full_name", "password","role","department","is_verified","is_staff")}),
+        ("Group Permissions", {"fields": ("groups", "user_permissions")}),
+        ("Last login", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "role",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = ("username","full_name")
+    ordering = ("full_name","is_verified")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Department) 
